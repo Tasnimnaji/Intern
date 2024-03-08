6980ff00ab9bbc8086df236b6a96cab0573da36f
Tasnim NAJI <98428439+Tasnimnaji@users.noreply.github.com>
2024-03-08 01:59:48 +0100

    updates

---
+++ test0/app/collection.py
@@ -0,0 +1,21 @@
+import os
+import pysftp
+from app.config import Config
+
+def collect_xml_files():
+    try:
+        xml_files = []
+
+        with pysftp.Connection(
+            host=Config.SFTP_HOST,
+            username=Config.SFTP_USERNAME,
+            password=Config.SFTP_PASSWORD,
+            port=Config.SFTP_PORT
+        ) as sftp:
+            sftp.chdir(Config.SFTP_COLLECTION_DIRECTORY)
+            xml_files = sftp.listdir()
+
+        return xml_files
+    except Exception as e:
+        print(f"Error collecting XML files: {e}")
+        return []
