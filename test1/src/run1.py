d5fbb8cb2644ae2353597833364dbf7afcc61576
Tasnim NAJI <98428439+Tasnimnaji@users.noreply.github.com>
2024-03-15 04:59:41 +0100

    Update run1.py

--- test1/src/run1.py
+++ test1/src/run1.py
@@ -1,7 +1,7 @@
 #This module contains code to run a Flask application.
-from app.views import app
 import sys
 sys.path.append('.')
+from app.views import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
