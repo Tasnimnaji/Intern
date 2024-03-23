a2c656d607a15a0753bc6312910f9e625cc7ded6
Tasnim NAJI <98428439+Tasnimnaji@users.noreply.github.com>
2024-03-23 06:33:52 +0100

    Update setup.py

--- test1/src/setup.py
+++ test1/src/setup.py
@@ -4,55 +4,44 @@ import datetime
 from setuptools import setup, find_packages
 
 
-# Get the absolute path to the directory containing setup.py
-base_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Specify the full path to requirements.txt
-requirements_file = os.path.join(base_dir, 'requirements.txt')
-
-# Read the contents of requirements.txt for dependencies
-with open(requirements_file, encoding='utf-8') as f:
-    requirements = f.read().splitlines()
-
-# Define a function to generate the snapshot version
 def generate_snapshot_version():
+    """
+    Generate a snapshot version based on the current date and time.
+    Returns:
+        str: Snapshot version string.
+    """
     now = datetime.datetime.now()
     version = now.strftime("%Y%m%d%H%M%S")
     return version + "-SNAPSHOT"
 
-# Define the upload_to_nexus function
+
 def upload_to_nexus():
+    """
+    Upload the package to Nexus repository.
+    """
     nexus_url = 'http://192.168.33.10:8081/repository/app/'
     username = 'admin'
     password = 'nexus'
 
-    # Generate the snapshot version
     snapshot_version = generate_snapshot_version()
 
-    # Update version in setup.py dynamically
     with open('setup.py', 'r', encoding='utf-8') as file:
         setup_content = file.read()
     setup_content = setup_content.replace("version='1.0.0.dev0'", f"version='{snapshot_version}'")
     with open('setup.py', 'w', encoding='utf-8') as file:
         file.write(setup_content)
 
-    # Command to create the source distribution package
     publish_command = ['python3', 'setup.py', 'sdist']
-
-    # Execute the publish command
     subprocess.run(publish_command, check=True)
 
-    # Command to publish the package to Nexus using twine
     upload_command = ['twine', 'upload', '--repository-url', nexus_url, '-u', username, '-p', password, '--skip-existing', 'dist/*']
-
-    # Execute the upload command
     subprocess.run(upload_command, check=True)
 
-# Call the upload_to_nexus function if UPLOAD_PACKAGE environment variable is set to 1
+
 if os.getenv('UPLOAD_PACKAGE') == '1':
     upload_to_nexus()
 
-# Setup function to define the package metadata
+
 setup(
     name='test1',
     version='1.0.0.dev0',
@@ -61,5 +50,5 @@ setup(
     description='End of Year project',
     packages=find_packages(),
     install_requires=requirements,
-    data_files=[('', ['requirements.txt'])],  # Include requirements.txt in the package
+    data_files=[('', ['requirements.txt'])],
 )
