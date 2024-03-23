494a5c6456c1ec020aa65f2c0feafe9ed9fe3f00
Tasnim NAJI <98428439+Tasnimnaji@users.noreply.github.com>
2024-03-23 06:26:15 +0100

    Update setup.py

--- test1/src/setup.py
+++ test1/src/setup.py
@@ -1,9 +1,7 @@
 import os
-import datetime  
+import datetime
 import subprocess
 from setuptools import setup, find_packages
-
-
 
 # Get the absolute path to the directory containing setup.py
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +10,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 requirements_file = os.path.join(base_dir, 'requirements.txt')
 
 # Read the contents of requirements.txt for dependencies
-with open(requirements_file) as f:
+with open(requirements_file, 'r', encoding='utf-8') as f:
     requirements = f.read().splitlines()
 
 # Define a function to generate the snapshot version
@@ -31,10 +29,10 @@ def upload_to_nexus():
     snapshot_version = generate_snapshot_version()
 
     # Update version in setup.py dynamically
-    with open('setup.py', 'r') as file:
+    with open('setup.py', 'r', encoding='utf-8') as file:
         setup_content = file.read()
     setup_content = setup_content.replace("version='1.0.0.dev0'", f"version='{snapshot_version}'")
-    with open('setup.py', 'w') as file:
+    with open('setup.py', 'w', encoding='utf-8') as file:
         file.write(setup_content)
 
     # Command to create the source distribution package
