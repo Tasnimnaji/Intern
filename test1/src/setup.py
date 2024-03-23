f51a55f877eca43f101db3187fe62cf792363f74
Tasnim NAJI <98428439+Tasnimnaji@users.noreply.github.com>
2024-03-23 06:24:20 +0100

    Update setup.py

--- test1/src/setup.py
+++ test1/src/setup.py
@@ -1,7 +1,8 @@
 import os
 import datetime  
-from setuptools import setup, find_packages
 import subprocess
+from setuptools import setup, find_packages
+
 
 
 # Get the absolute path to the directory containing setup.py
